d693e0bed9a1b0dbb6b925aca4b472618b85d492
William Correa <william.correa@picpay.com>
2026-02-26 04:50:03 +0000

    chore: update Cask to v0.8.0

--- Casks/arandu.rb
+++ Casks/arandu.rb
@@ -1,12 +1,12 @@
 cask "arandu" do
-  version "0.7.0"
+  version "0.8.0"
 
   on_arm do
-    sha256 "4b8d59365dd71f06468a835b715ef3ee97b67bc3432be52fb5c30d169f80d871"
+    sha256 "de2148c291e40a2a942ac81dd5fe89fc3973a40c1ff54687e86cd79606e25ee1"
     url "https://github.com/devitools/arandu/releases/download/v#{version}/Arandu_#{version}_aarch64.dmg"
   end
   on_intel do
-    sha256 "1c507f7cde70567a61fb40de5337d2ac99b24d8ed3c46393cf8e95305c2068e9"
+    sha256 "588b05da7a6cf6427240e8e329862f6f97d25a3924f0e8541da54758ccfa0b8c"
     url "https://github.com/devitools/arandu/releases/download/v#{version}/Arandu_#{version}_x64.dmg"
   end
 
